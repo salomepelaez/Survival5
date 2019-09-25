9c585251abb68bb18c817fc13f1c0d59f293e0fa
Salomé Pelaez <spsalome@gmail.com>
2019-09-25 17:25:15 -0500

    Weapon prefab added

--- Survival5/Assets/Scripts/Weapon.cs
+++ Survival5/Assets/Scripts/Weapon.cs
@@ -6,11 +6,22 @@ using TMPro;
 public class Weapon : MonoBehaviour
 {
     public TextMeshProUGUI message;
+    public GameObject stick;
 
     public void Start()
     {
-        message = GameObject.Find("Weapon").GetComponent<TextMeshProUGUI>();
-        GetComponent<Renderer>().material.color = Color.cyan;
+        Vector3 posicion = new Vector3();
+        posicion.x = -20;
+        posicion.y = -0.5f;
+        posicion.z = 20;
+
+        GameObject s = Instantiate(stick, posicion, Quaternion.identity);
+        s.GetComponent<Renderer>().material.color = Color.cyan;
+           
+        if (Input.GetKey(KeyCode.E))
+        {
+            Destroy(gameObject.GetComponent<Weapon>());
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
